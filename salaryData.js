cf283bd8d5f9bc6d604819b197810ed77cf3ecca
Md Akbar Hossain <75318851+ahzihan@users.noreply.github.com>
2022-04-10 15:28:02 +0600

    Update salaryData.js

--- salaryData.js
+++ salaryData.js
@@ -1,16 +1,16 @@
 let salary_data = {
   0: {
     hobby: "talking javascript",
-    name: "Addy Osmani",
+    name: "Md Alamin",
     salary: "76340.0",
   },
   1: {
-    name: "Paul Irish",
+    name: "Kazi Emon",
     salary: "94320.0",
     status: "married",
   },
   2: {
-    name: "Pete Hunt",
+    name: "Md Shakil",
     salary: "67000.0",
   },
   3: {
@@ -18,47 +18,47 @@ let salary_data = {
     salary: "105540.0",
   },
   4: {
-    name: "Remy Sharp",
+    name: "Nahid Emon",
     salary: "82220.0",
   },
   5: {
-    name: "David Beckham",
+    name: "Arif Billah",
     salary: "64430.0",
     sleepHour: 4,
   },
   6: {
     favLibrary: "React",
-    name: "A Random Guy",
+    name: "Kismot Ali",
     pizzaSlice: 5,
     salary: "36000.0",
   },
   7: {
-    name: "JS Dude",
+    name: "Uzzal",
     salary: "44432",
   },
   "-JvacZOcfUEvub7mxeo-": {
-    name: "Jhankar Mahbub",
+    name: "Md Akbar Hossain",
     organization: "Nielsen",
     salary: "53454",
   },
   "-Jvac_s61TL9oYM5SjMR": {
-    name: "Jhankar Mahbub",
+    name: "Jahanur",
     salary: "43454",
   },
   "-JvacbMTxlHCG5VaVDWg": {
-    name: "Jhankar Mahbub",
+    name: "Fokrul Islam",
     salary: "33454",
   },
   "-JviNyklLgZqk4rSB8pA": {
-    name: "Jhankar Mahbub",
+    name: "Shahin",
     salary: "23444",
   },
   "-JviO0Y_tNNMQNrdZVBR": {
-    name: "Jhankar Mahbub",
+    name: "Bijoy Debnath",
     salary: "63444",
   },
   "-JviO3gXUpSdnZHtliWg": {
-    name: "Jhankar Mahbub",
+    name: "AH Zihan",
     salary: "53655",
   },
 };
